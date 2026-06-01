f6b61aa0247d700b1362d747c0526cba3596f9e9
mobhTml <157970042+mobhTml@users.noreply.github.com>
2026-06-01 17:57:23 -0300

    001

---
+++ ex001.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int  main(){
+    int idade = 21;
+    float altura = 1.75;
+    char opcao = 'b';
+    char nome[20] = "Ryan";
+
+
+    //printf("a idade dp %s é: %d\n",nome,idade);
+    printf("o nome é: %s\n",nome);
+    printf("Sua idade é: %d\n",idade);
+    printf("a altura é; %.2f\n",altura);
+    printf("a opcao é:%C\n",opcao);
+
+
+    return 0;
+}
