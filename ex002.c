71ab0426684a7efab3409632f5609db9383f1d91
mobhTml <157970042+mobhTml@users.noreply.github.com>
2026-06-01 18:16:22 -0300

    projeto

--- ex002.c
+++ ex002.c
@@ -1,2 +1,35 @@
 #include <stdio.h>
 
+
+int main(){
+ // VARIAVEIS DA CARTA 1
+ char estado1;
+ char codigo1[10];
+ char pais1[20];
+ int populacao1;
+ float area1;
+ float pib1;
+ int pontos_turisticos1;
+
+// VARIAVEIS DA CARTA 2
+
+char estado2;
+char codigo2[10];
+char pais2[20];
+int populacao2;
+float area2;
+float pib2;
+int pontos_turisticos2;
+
+// area para entrada dos dados
+
+// CADASTRO DA CARTA 1
+
+printf("DIgite os dados da carta 1:\n");
+printf("Estado (A-H): ");
+scanf(" %c", estado1);
+
+ return 0;
+
+
+}
